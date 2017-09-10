81c04aee151339b65e38582ea3c5b3ec14968feb
Bryan Stephens <b-money@Bryans-MBP.domain>
2017-09-10 14:58:36 -0700

    Adding assignment 6 solution

---
+++ assignment-6/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <app-header></app-header>
+        <hr>
+        <app-server-status-board></app-server-status-board>
+        <hr>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+  import Header from './components/Header.vue'
+  import ServerStatusBoard from './components/ServerStatusBoard.vue'
+  import Footer from './components/Footer.vue'
+  
+  export default {
+    components: {
+      'app-header': Header,
+      'app-server-status-board': ServerStatusBoard,
+      'app-footer': Footer
+    }
+  }
+</script>
+
+<style>
+
+</style>
