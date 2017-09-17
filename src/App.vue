529e13ab76315171c3717ddc7fb2ee9755cef02c
Bryan Stephens <bryan.craig.stephens@gmail.com>
2017-09-17 12:33:38 -0700

    Assignment 8: dynamic components and slots

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,38 @@
 <template>
     <div class="container">
-        <app-header></app-header>
-        <hr>
-        <app-server-status-board></app-server-status-board>
-        <hr>
-        <app-footer></app-footer>
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button class="btn btn-primary" @click="selectedComponent = 'appBlue'">Load Blue Template</button>
+                <button class="btn btn-success" @click="selectedComponent = 'appGreen'">Load Green Template</button>
+                <button class="btn btn-danger" @click="selectedComponent = 'appRed'">Load Red Template</button>
+                <hr>
+                <keep-alive>
+                  <component :is="selectedComponent">Some Content</component>
+                </keep-alive>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-  import Header from './components/Header.vue'
-  import ServerStatusBoard from './components/ServerStatusBoard.vue'
-  import Footer from './components/Footer.vue'
-  
-  export default {
-    components: {
-      'app-header': Header,
-      'app-server-status-board': ServerStatusBoard,
-      'app-footer': Footer
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
+    export default {
+        data() {
+          return {
+            selectedComponent: "appBlue"
+          }
+        },
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        }
     }
-  }
 </script>
 
 <style>
-
 </style>
